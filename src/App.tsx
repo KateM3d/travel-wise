b257c045f95bffe66b13d8ece3d21722fbe5503d
KateM3d <ekaterina8medvedeva@gmail.com>
2024-07-01 20:22:33 -0400

    add home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import AppLayout from "./pages/AppLayout";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
 const App = () => {
-  return <div>Travel-wise</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/app" element={<AppLayout />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
