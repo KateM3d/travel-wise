8d54d88270be5b159f60914a8c6b04d1525d4d85
KateM3d <ekaterina8medvedeva@gmail.com>
2024-07-14 18:54:23 -0400

    add cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CityList from "./components/CityList/CityList";
 import AppLayout from "./pages/AppLayout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import { City } from "./types";
+
+const BASE_URL = "http://localhost:8000";
 
 const App: React.FC = () => {
+  const [cities, setCities] = useState<City[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -13,9 +36,14 @@ const App: React.FC = () => {
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<CityList />} />
-
-            <Route path="cities" element={<p>cities</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>form</p>} />
           </Route>
--- src/components/CityItem/CityItem.tsx
+++ src/components/CityItem/CityItem.tsx
@@ -1,5 +1,28 @@
-const CityItem: React.FC = () => {
-  return <div>cityItem</div>;
+import { City } from "../../types";
+import styles from "./CityItem.module.css";
+interface CityItemProps {
+  city: City;
+}
+
+const CityItem: React.FC<CityItemProps> = ({ city }) => {
+  const { cityName, emoji, date } = city;
+
+  const formatDate = (date: string) => {
+    return new Intl.DateTimeFormat("en", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }).format(new Date(date));
+  };
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
 };
 
 export default CityItem;
